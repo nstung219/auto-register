8a9f2e6cff61280b6839572708221e3995735360
TungNguyenSi <tung.nguyensi@nttdata.com>
2024-04-15 18:21:21 +0700

    update

--- src/main/java/att/App.java
+++ src/main/java/att/App.java
@@ -1,15 +1,5 @@
 package att;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import multithreading.Task;
-
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Hello world!
  *
